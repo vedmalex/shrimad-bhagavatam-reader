b51bed657270b0b94e61dbe11b78c64d00699d33
vedanta-krit.das <vedmalex@gmail.com>
2019-11-18 17:16:37 +0300

    search engine

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,6 +1,15 @@
 import { ApolloServer, gql } from 'apollo-server-micro';
 import SB from '../../lib/SB3-chapters.json';
+import sbIndex from '../../lib/SB3-index.json';
+import sbIndexResults from '../../lib/SB3-search-results.json';
 import SBTexts from '../../lib/SB3-texts.json';
+import lunr from 'lunr';
+
+import GraphQLJSON, { GraphQLJSONObject } from 'graphql-type-json';
+require('lunr-languages/lunr.stemmer.support')(lunr);
+require('lunr-languages/lunr.ru')(lunr);
+
+var idx = lunr.Index.load(sbIndex);
 
 function getTextSize(t) {
   var sanskrit = 0;
@@ -42,13 +51,16 @@ function getTextSize(t) {
     translation,
     purport,
     footnote,
-    overall: sanskrit + wbw + translation + (purport || 0) + (footnote || 0)
+    overall: sanskrit + wbw + translation + (purport || 0) + (footnote || 0),
   };
 }
 
 const typeDefs = gql`
+  scalar JSON
+  scalar JSONObject
+
   type Query {
-    env: String
+    search(text: String!): [VerseSearch]
     chapters: [Chapter]
     chaptersByNum(num: Int): Chapter
     chapterSize(num: Int!): ChapterSize
@@ -107,11 +119,28 @@ const typeDefs = gql`
     footnote: String
     wordsCount: verseSize
   }
+  type VerseSearch {
+    score: Float
+    metadata: JSON
+    id: String!
+    chapNo: Int
+    chapter: Chapter
+    text: [Int!]
+    name: String
+    sanskrit: [String]
+    wbw: [[String]]
+    translation: String
+    purport: [String]
+    footnote: String
+    wordsCount: verseSize
+  }
 `;
 
 const resolvers = {
+  JSON: GraphQLJSON,
+  JSONObject: GraphQLJSONObject,
   Query: {
-    env: () => JSON.stringify(process.env),
+    // env: () => JSON.stringify(process.env),
     chaptersByNum(_, { num }) {
       return SB.find(ch => num === ch.number);
     },
@@ -121,12 +150,19 @@ const resolvers = {
       const verse = last.text[last.text.length - 1];
       return {
         chapter: num,
-        verseCount: verse
+        verseCount: verse,
       };
     },
     chapters() {
       return SB;
-    }
+    },
+    search(_, { text }) {
+      return idx.search(text).map(res => ({
+        ...sbIndexResults[res.ref],
+        score: res.score,
+        metadata: res.matchData,
+      }));
+    },
     // chaptersByInterval(_, { filter }) {
     //   return [...SB.filter(ch => num == ch.number)];
     // },
@@ -137,7 +173,7 @@ const resolvers = {
       var last = root.texts[root.texts.length - 1];
       return last.text[last.text.length - 1];
     },
-    texts: root => SBTexts.filter(t => t.chapter == root.number)
+    texts: root => SBTexts.filter(t => t.chapter == root.number),
   },
   Verse: {
     id: item => `3.${item.chapter}.${item.name}`,
@@ -145,16 +181,16 @@ const resolvers = {
     chapter: root => SB.find(ch => ch.number == root.chapter),
     wordsCount: verse => {
       return getTextSize(verse);
-    }
-  }
+    },
+  },
 };
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
 export const config = {
   api: {
-    bodyParser: false
-  }
+    bodyParser: false,
+  },
 };
 
 export default apolloServer.createHandler({ path: '/api/graphql' });
